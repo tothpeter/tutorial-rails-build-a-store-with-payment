6c62f00fef270af6aaceb4a5fe83bca113172378
Peter Toth <tothpeter08@gmail.com>
2015-03-08 21:34:05 +0100

    2.3-Implementing a Cart: Part 2

--- app/models/cart.rb
+++ app/models/cart.rb
@@ -1,8 +1,16 @@
 class Cart
   attr_reader :items
 
-  def initialize
-    @items = []
+  def initialize items = []
+    @items = items
+  end
+
+  def self.build_from_hash hash
+    items = hash["cart"]["items"].map do |item_data|
+      CartItem.new item_data["product_id"], item_data["quantity"]
+    end
+
+    new items
   end
 
   def add_item product_id
@@ -19,7 +27,18 @@ class Cart
     @items.empty?
   end
 
-  def product
-    
+  def serialize
+    items = @items.map do |item|
+      {
+        "product_id" => item.product_id,
+        "quantity" => item.quantity
+      }
+    end
+
+    {
+      "cart" => {
+        "items" => items
+      }
+    }
   end
 end
