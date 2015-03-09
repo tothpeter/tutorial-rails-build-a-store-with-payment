b8c5a897ef5d7c4775e0756468a635e9d1978d86
Peter Toth <tothpeter08@gmail.com>
2015-03-09 21:53:30 +0100

    3.2-Creating a Transaction

---
+++ test/services/order_transaction_test.rb
@@ -0,0 +1,16 @@
+require 'test_helper'
+
+class OrderTransactionTest < Minitest::Test
+  include FactoryGirl::Syntax::Methods
+
+  def test_creates_a_transaction 
+    order = Order.new
+    order.order_items << OrderItem.new(product: build(:product), quantity: 1)
+    nonce = Braintree::Test::Nonce::Transactable
+    transaction = OrderTransaction.new order, nonce
+
+    transaction.execute
+
+    assert transaction.ok?, "Expected transaction to be successful."
+  end
+end
